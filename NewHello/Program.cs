a03ff6d8010d5997f024f8a02140e21b6b0ab164
Sarah Montoro <smontoro@price.usc.edu>
2020-07-14 10:23:47 -0700

    First commit for me!

--- NewHello/Program.cs
+++ NewHello/Program.cs
@@ -7,6 +7,7 @@ namespace NewHello
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What up!");
         }
     }
 }
