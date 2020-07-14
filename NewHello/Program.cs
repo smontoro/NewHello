54d7ccbcee6d3c8bbb9f93796a936f06bee7c734
Sarah Montoro <smontoro@price.usc.edu>
2020-07-14 10:35:07 -0700

    user 1 made this change

--- NewHello/Program.cs
+++ NewHello/Program.cs
@@ -8,6 +8,7 @@ namespace NewHello
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("What up!");
+            Console.WriteLine("User1 made this change");
         }
     }
 }
